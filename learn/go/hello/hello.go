e62210f703b1ff48947271737b62cd8980c218ff
acevest <zhaoyanbai@126.com>
2015-01-04 17:41:31 +0800

    ...

--- learn/go/hello/hello.go
+++ learn/go/hello/hello.go
@@ -1,7 +1,12 @@
 package main
 
 import "fmt"
+import "math/rand"
+import "time"
 
 func main() {
-	fmt.Printf("hello world\n")
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < rand.Intn(10); i++ {
+		fmt.Printf("Hello World\n")
+	}
 }
