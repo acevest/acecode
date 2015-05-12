9721381cc7a2196fd81404b05d79a3fb31d92579
acevest <zhaoyanbai@126.com>
2015-05-12 16:55:06 +0800

    ...

---
+++ learn/server/server.cpp
@@ -0,0 +1,16 @@
+/*
+ * ------------------------------------------------------------------------
+ *   File Name: server.cpp
+ *      Author: Zhao Yanbai
+ *              Tue May 12 15:30:58 2015
+ * Description: none
+ * ------------------------------------------------------------------------
+ */
+#include<iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+	cout << "Server is Starting ... " << endl;
+	return 0;
+}
