8e0550a0734ecf72a370a43c4301483b69a3ab0c
AceVest <zhaoyanbai@126.com>
2015-12-30 12:09:41 +0800

    ...

--- tools/comm/dec2hex.py
+++ tools/comm/dec2hex.py
@@ -6,8 +6,28 @@
 #              Tue Apr 28 15:00:56 2015
 # Description: none
 # ------------------------------------------------------------------------
+import sys
 
-while True :
-	n = raw_input("Input Decimal: ")
-	hexs = "{0:X}".format(int(n))
-	print hexs
+def main() :
+    while True :
+        n = 0
+        inputdata = raw_input("> ")
+        if inputdata.lower() == 'quit' :
+            sys.exit()
+
+        try :
+            n = int(inputdata)
+        except ValueError,e :
+            print "ERROR: ", e
+            continue
+
+        hexs = "{0:X} 0x{1:X}".format(n, n)
+        print hexs
+
+
+if __name__ == "__main__" :
+    try :
+        main()
+    except KeyboardInterrupt :
+        print "Bye..."
+        pass
