9c69be2081db9578d5f20301193cfebd5dde52c0
AceVest <zhaoyanbai@126.com>
2018-06-14 20:31:49 +0800

    add ace arduino hardware

---
+++ arduino/hardware/ace/README.md
@@ -0,0 +1,1 @@
+https://github.com/arduino/Arduino/wiki/Arduino-IDE-1.5-3rd-party-Hardware-specification
---
+++ arduino/hardware/ace/avr/cores/avr/Arduino.h
@@ -0,0 +1,16 @@
+/*
+ * ------------------------------------------------------------------------
+ *   File Name: Arduino.h
+ *      Author: Zhao Yanbai
+ *              2018-06-14 20:01:07 Thursday CST
+ * Description: none
+ * ------------------------------------------------------------------------
+ */
+
+#pragma once
+
+void setup(void);
+void loop(void);
+
+
+#include <AceAvr.h>
