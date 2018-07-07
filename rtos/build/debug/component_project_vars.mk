acb19f971111c3253e21512fae6a07b0e8123827
AceVest <zhaoyanbai@126.com>
2018-07-07 15:15:48 +0800

    ...

--- rtos/build/debug/component_project_vars.mk
+++
@@ -1,7 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/components/debug/include
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/debug -ldebug
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += debug
-component-debug-build: 
--- rtos/build/test/component_project_vars.mk
+++
@@ -1,7 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/components/test/include
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/test -ltest
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += test
-component-test-build: 
