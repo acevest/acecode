8de9f1439bb2fc860905f9149d86fc728167509d
AceVest <zhaoyanbai@126.com>
2018-07-29 23:43:09 +0800

    ...

---
+++ rtos/kernel/include/FreeRTOSConfig.h
@@ -0,0 +1,21 @@
+/*
+ * ------------------------------------------------------------------------
+ *   File Name: FreeRTOSConfig.h
+ *      Author: Zhao Yanbai
+ *              2018-07-07 15:16:55 Saturday CST
+ * Description: none
+ * ------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#define configMAX_PRIORITIES        25
+#define configMINIMAL_STACK_SIZE    768
+#define configUSE_TICK_HOOK         0
+#define configUSE_IDLE_HOOK         0
+#define configUSE_PREEMPTION        1
+#define configUSE_16_BIT_TICKS      0
+#define configUSE_CO_ROUTINES       0
+
+
+
