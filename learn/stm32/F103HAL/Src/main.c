bcebefa85ae8fed86ef2035bceacb0226b192fe4
AceVest <zhaoyanbai@126.com>
2019-06-01 14:21:56 +0800

    F103HAL-支持Serial Wire调式

--- learn/stm32/F103HAL/Src/main.c
+++ learn/stm32/F103HAL/Src/main.c
@@ -20,6 +20,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -86,6 +87,7 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
