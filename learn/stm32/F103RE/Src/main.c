93d23558a9ea11f8265fbb960b5876130383f529
AceVest <zhaoyanbai@126.com>
2019-06-05 10:29:04 +0800

    F103RE GPIO

--- learn/stm32/F103RE/Src/main.c
+++ learn/stm32/F103RE/Src/main.c
@@ -96,6 +96,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
+	  HAL_Delay(100);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
